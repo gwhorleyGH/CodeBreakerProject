51c90ffcac800a86f1a09e49fbc5916ab9ffe263
Greg Whorley <greg@whorley.com>
2017-11-21 16:22:37 -0800

    final push, i had to watch the video :(

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,40 +3,36 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    if (answer.value === '' && attempt.value === '') {
-        setHiddenFields()
+    if (answer.value === '' || attempt.value === '') {
+        setHiddenFields();
     }
-    if (validateInput(input.value)) {
-        attempt.value++;
+    if (!validateInput(input.value)) {
+        return;
     }
-    else if (!validateInput(input.value)) {
-        return false
+    attempt.value++;
+
+    if (getResults(input.value)) {
+        setMessage('You Win! :)');
+        showAnswer(true);
+        showReplay();
     }
-    if (getResults(input)) {
-        setMessage('You Win! :)')
-        showAnswer(true)
-        showReplay()
+    else if (attempt.value >= 10) {
+        setMessage('You Lose! :(');
+        showAnswer(false);
+        showReplay();
     }
-    else if (!getResults(input) && attempt.value >= 10) {
-        setMessage('You Lose! :(')
-        showAnswer(false)
-        showReplay()
-    }
-    else if (!getResults(input) && attempt.value < 10) {
-        setMessage('Incorrect, try again.')
+    else {
+        setMessage('Incorrect, try again.');
     }
 }
 
 function setHiddenFields() {
     //set answer variable equal to a randomly generated whole number between 0 and 9999
-    let answerString = String(Math.floor(Math.random() * 9999));
-    let padding = '';
-     if (answerString.length < 4) {
-        let howMany = 4 - answerString.length;
-        padding = '0'.repeat(howMany)
+    answer.value = Math.floor(Math.random() * 10000).toString();
+     while (answer.value.length < 4) {
+        answer.value = "0" + answer.value
     }
-    answer.value = padding.concat(answerString);
-    attempt = 0;
+    attempt.value = 0;
 }
 
 function setMessage(messageText) {
@@ -46,44 +42,38 @@ function setMessage(messageText) {
 function validateInput(inputText) {
     if (inputText.length !== 4) {
         setMessage('Guesses must be exactly 4 characters long.');
-        return false
+        return false;
     }
-    else {
-        return true
-    }
+    return true;
 }
 
 function getResults(input) {
-    let initialDiv = '<div class="row"><span class="col-md-6">' + input.value + '</span><div class="col-md-6">';
-    let correctChars = 0;
+    let initialDiv = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
     for (let i = 0; i < input.length; i++) {
-        if (input.value.charAt(i) === answer.value.charAt(i)) {
-            initialDiv += '<span class="glyphicon glyphicon-ok">' + input.value.charAt(i) + '</span>';
-            correctChars++;
+        if (input.charAt(i) === answer.value.charAt(i)) {
+            initialDiv += '<span class="glyphicon glyphicon-ok"></span>';
         }
-        else if (input.value.charAt(i) !== answer.value.charAt(i) && answer.value.includes(input.value.charAt(i))) {
-            initialDiv += '<span class="glyphicon glyphicon-transfer">' + input.value.charAt(i) + '</span>';
+        else if (answer.value.indexOf(input.charAt(i)) > -1) {
+            initialDiv += '<span class="glyphicon glyphicon-transfer"></span>';
         }
-        else if (!answer.value.includes(input.value.charAt(i))) {
-            initialDiv += '<span class="glyphicon glyphicon-remove">' + input.value.charAt(i) + '</span>';
+        else {
+            initialDiv += '<span class="glyphicon glyphicon-remove"></span>';
         }
     }
     initialDiv += '</div></div>';
-    document.getElementById('results').innerHTML = initialDiv;
-    return correctChars === input.length
+    document.getElementById('results').innerHTML += initialDiv;
+    return input === answer.value;
 }
 
 function showAnswer(youWin) {
-    /*
-    This function should set the innerHTML of the code label to the value of the answer hidden input. In addition to this it should take the parameter as a true or false (indicating if the player won or lost) if the parameter is true add success to code's className otherwise it should add failure. (note the space before success and failure)
-     */
-    document.getElementById('code').innerHTML = answer.value;
+    let code = document.getElementById('code')
     if (youWin) {
-        document.getElementById('code').className = ' success';
+        code.className = ' success';
     }
     else {
-        document.getElementById('code').className = ' failure';
+        code.className = ' failure';
     }
+    code.innerHTML = answer.value;
 }
 
 function showReplay() {
